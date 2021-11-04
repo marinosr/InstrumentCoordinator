31ceaf3d1ace4055085f78f995f633841287d033
Richard E. Marinos <marinosr@gmail.com>
2021-11-04 08:56:00 -0400

    Bugfix  liaison arduino control

--- Source/Arduino_LiaisonFirmware/Arduino_LiaisonFirmware.ino
+++ Source/Arduino_LiaisonFirmware/Arduino_LiaisonFirmware.ino
@@ -13,15 +13,15 @@ void loop() {
   switch(buffer[2]){ // This reads the second character in the buffer to determine which valve wants to be switched.
     case '1':
     digitalWrite(4, state); //Sets the pin to the desired state. 
-    serial.write('OK');
+    serial.write('OK\n');
     break;
     case '2':
     digitalWrite(5, state);
-    serial.write('OK');
+    serial.write('OK\n');
     break;
     case '3':
     digitalWrite(6, state);
-    serial.write('OK');
+    serial.write('OK\n');
     break;
     case '4':
     if(state=35){ // If the 4th character of read command is 'S', ('S'-'0'=35) then read what the current state of the pin is, and set the desired state to the opposite.'S' is 'switch'
@@ -29,12 +29,12 @@ void loop() {
       state = !(current);
     }
     digitalWrite(7, state);
-    serial.write('OK');
+    serial.write('OK\n');
     break;
   }
   break;
   case 'R':
-  serial.write('Liaison ready.');
+  serial.write('Liaison ready.\n');
   break;
       
     }
