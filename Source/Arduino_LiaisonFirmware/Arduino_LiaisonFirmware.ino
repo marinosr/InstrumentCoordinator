fd1f4f26c17e30d061ff7ea3747d98cf05226a70
Yuan Ren <yren@picarro.com>
2022-02-28 20:59:57 +0000

    arduno lia update

--- Source/Arduino_LiaisonFirmware/Arduino_LiaisonFirmware.ino
+++ Source/Arduino_LiaisonFirmware/Arduino_LiaisonFirmware.ino
@@ -1,4 +1,4 @@
-int currentseven = 0;
+int currentseven = 1;
 
 void setup() {
     Serial.begin(9600);
@@ -6,17 +6,23 @@ void setup() {
     pinMode(5, OUTPUT);
     pinMode(6, OUTPUT);
     pinMode(7, OUTPUT);
+    digitalWrite(4,1);
+    digitalWrite(5,1);
+    digitalWrite(6,1);
+    digitalWrite(7,1);
 }
 
 void loop() {
  char buffer[8];
  while(!Serial.available()); // Wait until there is serial data received. 
  int size = Serial.readBytesUntil('\n', buffer, 8); // Read up to 8 bytes of serial data, until '\n' is received
- Serial.write(buffer, 4);
+ Serial.write(buffer, size);
+
+int state = 0;
  
  switch(buffer[0]){
   case 'V':  //If requesting a valve switch
-  int state = buffer[3] - '0'; // Determine whether it should be off or on. (The -'0' converts from character to integer)
+  state = buffer[3] - '0'; // Determine whether it should be off or on. (The -'0' converts from character to integer)
   switch(buffer[1]){ // This reads the second character in the buffer to determine which valve wants to be switched.
     case '1':
     digitalWrite(4, state); //Sets the pin to the desired state. 
@@ -33,7 +39,11 @@ void loop() {
     case '4':
     Serial.write(state);
    if(state==35){ // If the 4th character of read command is 'S', ('S'-'0'=35) then read what the current state of the pin is, and set the desired state to the opposite.'S' is 'switch'
-      state = !(currentseven);
+      if(currentseven==1){
+        state = 0;
+      } else {
+        state = 1;
+      }
     }
     digitalWrite(7, state);
     currentseven = state; 
